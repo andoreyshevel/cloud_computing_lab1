dcb8e238d6a2a83972b35b0b5e420ae97a3da4ae
Andrii Shevel <andrii.shevel@megogo.net>
2023-12-22 06:16:46 +0200

    change app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+
+const TimerToSaturday = () => {
+  const calculateTimeLeft = () => {
+    const now = new Date();
+    const nextSaturday = new Date(
+      now.getFullYear(),
+      now.getMonth(),
+      now.getDate() + ((6 - now.getDay() + 7) % 7),
+      0, 0, 0
+    );
+    return nextSaturday.getTime() - now.getTime();
+  };
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  const formatTimeLeft = () => {
+    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeLeft / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((timeLeft / 1000 / 60) % 60);
+    const seconds = Math.floor((timeLeft / 1000) % 60);
+    return `${days} днів ${hours} годин ${minutes} хвилин ${seconds} секунд`;
+  };
+
+  return (
+    <div>
+      <h1>Час до наступної суботи</h1>
+      <div>{formatTimeLeft()}</div>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TimerToSaturday />
     </div>
   );
 }
